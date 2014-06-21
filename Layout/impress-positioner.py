640c251f7c0c26497e97e4de9e028955852ba460
Ben Nizette <bennizette@gmail.com>
2014-06-21 12:07:29 +0800

    Initial: Layout engine

---
+++ Layout/impress-positioner.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import sys, re
+
+fin = sys.argv[1]
+fout = fin[:fin.rfind('.')] + '-lay.html'
+
+xloc = 0
+yloc = 0
+
+xmin = 0
+xmax = 1
+ymin = 0
+ymax = 1
+
+def process1(l):
+	global xloc, yloc, xmin, xmax, ymin, ymax
+	need_rewrite = False
+
+	# Track current X position
+	xm = re.search('data-x=.([\-0-9]*).',l)
+	if xm is not None:
+		xloc = int(xm.group(1))
+
+	xm = re.search('data-x-rel=.([\-0-9]*).',l)
+	if xm is not None:
+		xloc += int(xm.group(1))
+		need_rewrite = True
+
+	# Track current Y position
+	xm = re.search('data-y=.([\-0-9]*).',l)
+	if xm is not None:
+		yloc = int(xm.group(1))
+
+	xm = re.search('data-y-rel=.([\-0-9]*).',l)
+	if xm is not None:
+		yloc += int(xm.group(1))
+		need_rewrite = True
+
+	# Rewrite pos
+	if need_rewrite:
+		xstr = "data-x='{}'".format(xloc)
+		ystr = "data-y='{}'".format(yloc)
+
+		l = re.sub('data-x-rel=.([\-0-9]*).', xstr, l)
+		l = re.sub('data-y-rel=.([\-0-9]*).', ystr, l)
+
+	xmin = min(xmin, xloc)
+	xmax = max(xmax, xloc)
+	ymin = min(ymin, yloc)
+	ymax = max(ymax, yloc)
+
+	return l
+
+def process2(l):
+	r = re.search('<div id=[\'"](over.*)[\'"] .*/>', l)
+	if r is not None:
+		idtag = r.group(1)
+		xscale = (1000 + xmax - xmin) / 1250.
+		yscale = (1000 + ymax - ymin) / 1000.
+		scale = max(xscale, yscale) 
+		# Rewrite the overview slide, if present
+		overview = '	<div id="{}" class="step" data-x="{}" data-y="{}" data-scale="{:.2}">&nbsp</div>'.format(
+			idtag,
+			(xmax - xmin) / 2,
+			(ymax - ymin) / 2 - (50 * scale),
+			scale
+			)
+		return overview
+
+	return l
+
+# Load and relative position processing
+fdata = []
+with open(fin, 'r') as f:
+	for l in f:
+		fdata.append(process1(l))
+
+# Second pass, global position info (e.g. overview slides)
+fdata2 = []
+for l in fdata:
+	fdata2.append(process2(l))
+
+with open(fout, 'w') as fo:
+	fo.write("".join(fdata2))
+
